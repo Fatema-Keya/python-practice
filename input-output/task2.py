18b14e4686d102791cdd6381506cd344b2170e5d
Fatema-Keya <fatemakeya05@gmail.com>
2026-06-16 16:07:39 +0600

    Task 2 done

---
+++ input-output/task2.py
@@ -0,0 +1,12 @@
+a = (int(input("Enter your First Number: ")))
+b = (int(input("Enter your second Number: ")))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Addition", sum)
+print("Subtraction", sub)
+print("Multiplication", mul)
+print("Division", div)
